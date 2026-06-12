742cde461835db0a027a94fa0d9fecacb0a93083
zoomdong <1344492820@qq.com>
2026-06-12 17:48:16 +0800

    feat: port rule no-inner-declarations

--- src/main.zig
+++ src/main.zig
@@ -148,6 +148,8 @@ pub fn main(init: std.process.Init) !void {
             options.no_irregular_whitespace = false;
         } else if (std.mem.eql(u8, arg, "--no-inline-comments=off")) {
             options.no_inline_comments = false;
+        } else if (std.mem.eql(u8, arg, "--no-inner-declarations=off")) {
+            options.no_inner_declarations = false;
         } else if (std.mem.eql(u8, arg, "--no-iterator=off")) {
             options.no_iterator = false;
         } else if (std.mem.eql(u8, arg, "--no-label-var=off")) {
@@ -513,6 +515,7 @@ fn printHelp() void {
         \\  --no-import-assign=off     Disable no-import-assign
         \\  --no-irregular-whitespace=off Disable no-irregular-whitespace
         \\  --no-inline-comments=off   Disable no-inline-comments
+        \\  --no-inner-declarations=off Disable no-inner-declarations
         \\  --no-iterator=off          Disable no-iterator
         \\  --no-label-var=off         Disable no-label-var
         \\  --no-labels=off           Disable no-labels
---
+++ tests/rules/no_inner_declarations.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+const lint = @import("utoo_lint");
+const helpers = @import("../helpers.zig");
+
+test "reports no-inner-declarations for function declarations inside blocks" {
+    const source =
+        \\if (foo) {
+        \\  function bar() {}
+        \\}
+        \\while (foo) {
+        \\  function baz() {}
+        \\}
+        \\{
+        \\  function qux() {}
+        \\}
+    ;
+
+    var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", .{
+        .no_constant_condition = false,
+        .no_lone_blocks = false,
+        .no_undef = false,
+        .no_unused_vars = false,
+        .parser_semantic_errors = false,
+    });
+    defer result.deinit(std.testing.allocator);
+
+    try std.testing.expectEqual(@as(usize, 3), helpers.countRule(result, lint.rules.no_inner_declarations.id));
+}
+
+test "does not report no-inner-declarations for root function declarations" {
+    const source =
+        \\function topLevel() {}
+        \\function outer() {
+        \\  function inner() {}
+        \\  const fn = function expression() {};
+        \\}
+        \\export function exported() {}
+    ;
+
+    var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", .{
+        .no_unused_vars = false,
+        .parser_semantic_errors = false,
+    });
+    defer result.deinit(std.testing.allocator);
+
+    try std.testing.expect(!helpers.hasRule(result, lint.rules.no_inner_declarations.id));
+}
+
+test "can disable no-inner-declarations" {
+    const source =
+        \\if (foo) {
+        \\  function bar() {}
+        \\}
+    ;
+
+    var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", .{
+        .no_inner_declarations = false,
+        .no_undef = false,
+        .no_unused_vars = false,
+        .parser_semantic_errors = false,
+    });
+    defer result.deinit(std.testing.allocator);
+
+    try std.testing.expect(!helpers.hasRule(result, lint.rules.no_inner_declarations.id));
+}
