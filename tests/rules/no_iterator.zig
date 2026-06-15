247728a80101e828d08b210d31916be2a00128df
zoomdong <1344492820@qq.com>
2026-06-15 20:40:17 +0800

    fix: detect template iterator property (#591)

--- tests/rules/no_iterator.zig
+++ tests/rules/no_iterator.zig
@@ -8,6 +8,8 @@ test "reports no-iterator for iterator member access" {
         \\foo["__iterator__"] = bar;
         \\const iterator = foo.__iterator__;
         \\const iterator2 = foo["__iterator__"];
+        \\foo[`__iterator__`] = bar;
+        \\const iterator3 = foo[`__iterator__`];
     ;
 
     var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", .{
@@ -17,7 +19,7 @@ test "reports no-iterator for iterator member access" {
     });
     defer result.deinit(std.testing.allocator);
 
-    try std.testing.expectEqual(@as(usize, 4), helpers.countRule(result, lint.rules.no_iterator.id));
+    try std.testing.expectEqual(@as(usize, 6), helpers.countRule(result, lint.rules.no_iterator.id));
 }
 
 test "does not report no-iterator for object literal properties or other members" {
@@ -27,6 +29,7 @@ test "does not report no-iterator for object literal properties or other members
         \\  other: foo.__iter__,
         \\};
         \\foo[iterator] = bar;
+        \\foo[`__${name}__`] = bar;
     ;
 
     var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", .{
