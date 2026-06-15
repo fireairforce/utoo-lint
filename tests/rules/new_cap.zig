3030d85d351f0b8c6f0d298883d83f750d2d2418
zoomdong <1344492820@qq.com>
2026-06-16 01:04:57 +0800

    fix: detect template new-cap members (#603)

--- tests/rules/new_cap.zig
+++ tests/rules/new_cap.zig
@@ -9,6 +9,8 @@ test "reports new-cap for lowercase constructors and uppercase function calls" {
         \\Foo();
         \\namespace.Foo();
         \\namespace["Foo"]();
+        \\new namespace[`foo`]();
+        \\namespace[`Foo`]();
     ;
 
     var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", .{
@@ -19,14 +21,16 @@ test "reports new-cap for lowercase constructors and uppercase function calls" {
     });
     defer result.deinit(std.testing.allocator);
 
-    try std.testing.expectEqual(@as(usize, 5), helpers.countRule(result, lint.rules.new_cap.id));
+    try std.testing.expectEqual(@as(usize, 7), helpers.countRule(result, lint.rules.new_cap.id));
 }
 
-test "does not report new-cap for conventional constructor usage or callable builtins" {
+test "does not report new-cap for conventional constructor usage callable builtins or dynamic members" {
     const source =
         \\new Foo();
         \\foo();
         \\new namespace.Foo();
+        \\new namespace[`fo${suffix}`]();
+        \\namespace[`Fo${suffix}`]();
         \\Array();
         \\Boolean();
         \\Date();
