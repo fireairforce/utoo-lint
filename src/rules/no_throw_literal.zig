f8e0093bcfa389b85ffcf9d9eb866747b065a970
zoomdong <1344492820@qq.com>
2026-06-16 07:03:54 +0800

    Align no-throw-literal unary throws (#635)

--- src/rules/no_throw_literal.zig
+++ src/rules/no_throw_literal.zig
@@ -41,6 +41,7 @@ fn isInvalidThrowArgument(tree: *const ast.Tree, index: ast.NodeIndex) bool {
         .array_expression,
         => true,
         .identifier_reference => |identifier| std.mem.eql(u8, tree.string(identifier.name), "undefined"),
+        .unary_expression => true,
         .binary_expression => |expression| expression.operator == .add,
         else => false,
     };
