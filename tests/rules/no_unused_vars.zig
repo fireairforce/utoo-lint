094e39b9db0a1914784f762647cc331712694231
zoomdong <1344492820@qq.com>
2026-06-17 09:39:52 +0800

    support no-unused-vars caughtErrors option (#718)

--- tests/rules/no_unused_vars.zig
+++ tests/rules/no_unused_vars.zig
@@ -71,3 +71,31 @@ test "supports configured no-unused-vars args all" {
 
     try std.testing.expectEqual(@as(usize, 2), helpers.countRule(result, lint.rules.no_unused_vars.id));
 }
+
+test "supports configured no-unused-vars caughtErrors none" {
+    var config = try std.json.parseFromSlice(
+        std.json.Value,
+        std.testing.allocator,
+        "[\"error\",{\"caughtErrors\":\"none\"}]",
+        .{},
+    );
+    defer config.deinit();
+
+    var options = lint.Options{};
+    try options.setByRuleConfigValue("no-unused-vars", config.value);
+    options.no_undef = false;
+    options.typescript_eslint_no_unused_vars = false;
+    options.parser_semantic_errors = false;
+
+    const source =
+        \\try {
+        \\  run();
+        \\} catch (unusedError) {
+        \\}
+    ;
+
+    var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", options);
+    defer result.deinit(std.testing.allocator);
+
+    try std.testing.expect(!helpers.hasRule(result, lint.rules.no_unused_vars.id));
+}
