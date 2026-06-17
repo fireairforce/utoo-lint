8efcede0f9d75f84b3fbf2b81f98ea34056fce61
zoomdong <1344492820@qq.com>
2026-06-17 17:49:08 +0800

    support object-shorthand style options (#727)

--- tests/rules/object_shorthand.zig
+++ tests/rules/object_shorthand.zig
@@ -66,6 +66,128 @@ test "does not report object-shorthand for non-shorthandable properties" {
     try std.testing.expect(!helpers.hasRule(result, lint.rules.object_shorthand.id));
 }
 
+test "supports configured object-shorthand methods style" {
+    const source =
+        \\const obj = {
+        \\  foo: foo,
+        \\  bar: function () {
+        \\    return 1;
+        \\  },
+        \\};
+    ;
+
+    var config = try std.json.parseFromSlice(
+        std.json.Value,
+        std.testing.allocator,
+        "[\"error\",\"methods\"]",
+        .{},
+    );
+    defer config.deinit();
+    var options = lint.Options{
+        .no_unused_vars = false,
+        .no_undef = false,
+        .parser_semantic_errors = false,
+    };
+    try options.setByRuleConfigValue("object-shorthand", config.value);
+
+    var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", options);
+    defer result.deinit(std.testing.allocator);
+
+    try std.testing.expectEqual(@as(usize, 1), helpers.countRule(result, lint.rules.object_shorthand.id));
+}
+
+test "supports configured object-shorthand properties style" {
+    const source =
+        \\const obj = {
+        \\  foo: foo,
+        \\  bar: function () {
+        \\    return 1;
+        \\  },
+        \\};
+    ;
+
+    var config = try std.json.parseFromSlice(
+        std.json.Value,
+        std.testing.allocator,
+        "[\"error\",\"properties\"]",
+        .{},
+    );
+    defer config.deinit();
+    var options = lint.Options{
+        .no_unused_vars = false,
+        .no_undef = false,
+        .parser_semantic_errors = false,
+    };
+    try options.setByRuleConfigValue("object-shorthand", config.value);
+
+    var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", options);
+    defer result.deinit(std.testing.allocator);
+
+    try std.testing.expectEqual(@as(usize, 1), helpers.countRule(result, lint.rules.object_shorthand.id));
+}
+
+test "supports configured object-shorthand never style" {
+    const source =
+        \\const obj = {
+        \\  foo,
+        \\  bar() {
+        \\    return 1;
+        \\  },
+        \\  baz: baz,
+        \\};
+    ;
+
+    var config = try std.json.parseFromSlice(
+        std.json.Value,
+        std.testing.allocator,
+        "[\"error\",\"never\"]",
+        .{},
+    );
+    defer config.deinit();
+    var options = lint.Options{
+        .no_unused_vars = false,
+        .no_undef = false,
+        .parser_semantic_errors = false,
+    };
+    try options.setByRuleConfigValue("object-shorthand", config.value);
+
+    var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", options);
+    defer result.deinit(std.testing.allocator);
+
+    try std.testing.expectEqual(@as(usize, 2), helpers.countRule(result, lint.rules.object_shorthand.id));
+}
+
+test "supports configured object-shorthand avoidQuotes option" {
+    const source =
+        \\const obj = {
+        \\  foo: foo,
+        \\  "quoted": quoted,
+        \\  "quoted-method": function () {
+        \\    return 1;
+        \\  },
+        \\};
+    ;
+
+    var config = try std.json.parseFromSlice(
+        std.json.Value,
+        std.testing.allocator,
+        "[\"error\",\"always\",{\"avoidQuotes\":true}]",
+        .{},
+    );
+    defer config.deinit();
+    var options = lint.Options{
+        .no_unused_vars = false,
+        .no_undef = false,
+        .parser_semantic_errors = false,
+    };
+    try options.setByRuleConfigValue("object-shorthand", config.value);
+
+    var result = try lint.lintSource(std.testing.allocator, source, "fixture.js", options);
+    defer result.deinit(std.testing.allocator);
+
+    try std.testing.expectEqual(@as(usize, 1), helpers.countRule(result, lint.rules.object_shorthand.id));
+}
+
 test "can disable object-shorthand" {
     const source =
         \\const obj = {
