696fc79ccff6939071fca66075cb9dfe44fce08e
zoomdong <1344492820@qq.com>
2026-06-20 20:26:08 +0800

    Register react no-unknown-property rule (#965)

--- npm/utoo-lint/index.js
+++ npm/utoo-lint/index.js
@@ -345,6 +345,7 @@ const BUILTIN_RULE_IDS = [
   "react/no-render-return-value",
   "react/no-string-refs",
   "react/no-unescaped-entities",
+  "react/no-unknown-property",
   "react/no-unused-prop-types",
   "react/prop-types",
   "react-hooks/rules-of-hooks",
